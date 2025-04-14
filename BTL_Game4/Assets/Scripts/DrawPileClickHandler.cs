546520909a7e33472642d493546130c02a3084ae
giomuatunhien <cuong.dong2210420@hcmut.edu.vn>
2025-04-14 23:51:08 +0700

    14/4/2025

--- BTL_Game4/Assets/Scripts/DrawPileClickHandler.cs
+++ BTL_Game4/Assets/Scripts/DrawPileClickHandler.cs
@@ -8,22 +8,23 @@ public class DrawPileClickHandler : MonoBehaviour, IPointerClickHandler
 {
     // Tham chiếu đến script CardDrawer (gán qua Inspector)
     public CardDrawer cardDrawer;
-    public OpponentHandManager opponentHandManager;
+    public PlayerHandManager opponentHandManager;
     void Start()
     {
         ///tesst opponent có thể xóa
-        if (opponentHandManager == null)
-        {
-            GameSetupManager gm = FindAnyObjectByType<GameSetupManager>();
-            if (gm != null && gm.opponentManagers.Count > 0)
-            {
-                opponentHandManager = gm.opponentManagers[0]; // ← Chọn đối thủ đầu tiên
-            }
-            if (opponentHandManager == null)
-            {
-                Debug.LogError("OpponentHandManager vẫn null sau khi khởi tạo.");
-            }
-        }
+        // if (opponentHandManager == null)
+        // {
+        //     GameSetupManager gm = FindAnyObjectByType<GameSetupManager>();
+        //     if (gm != null && gm.playerManagers.Count > 0)
+        //     {
+        //         opponentHandManager = gm.playerManagers[0]; // ← Chọn đối thủ đầu tiên
+        //     }
+        //     if (opponentHandManager == null)
+        //     {
+        //         Debug.LogError("OpponentHandManager vẫn null sau khi khởi tạo.");
+        //         Debug.Log(gm.playerManagers.Count);
+        //     }
+        // }
     }
 
     // Hàm xử lý sự kiện click
@@ -31,10 +32,11 @@ public class DrawPileClickHandler : MonoBehaviour, IPointerClickHandler
     {
         if (cardDrawer != null)
         {
-            CardData drawnCard = DeckManager.Instance.DrawCard();
+
             cardDrawer.DrawCard();
             Debug.Log("Đã click vào DrawPile, rút bài!");
-            opponentHandManager.SpawnCard(drawnCard);
+            //CardData drawnCard = DeckManager.Instance.DrawCard();
+            //opponentHandManager.SpawnCard(drawnCard);
         }
         else
         {
--- BTL_Game4/Assets/Scripts/GameSetupManager.cs
+++ BTL_Game4/Assets/Scripts/GameSetupManager.cs
@@ -8,30 +8,43 @@ public class GameSetupManager : MonoBehaviour
 
     [Header("References")]
     public PlayerHandManager playerHandManager;
-    public GameObject opponentHandPrefab;
-    public Transform opponentsParent;
+    // public GameObject opponentHandPrefab;
+    // public Transform opponentsParent;
 
-    public List<OpponentHandManager> opponentManagers = new();
+    //public List<OpponentHandManager> opponentManagers = new();
+    //public List<PlayerHandManager> playerManagers = new();
 
-    void Start()
+    void Awake()
     {
         CreateOpponents();
+    }
+    void Start()
+    {
+        //CreateOpponents();
         DealInitialCards();
     }
 
     void CreateOpponents()
     {
-        foreach (var data in opponentsData)
-        {
-            GameObject newOpponent = Instantiate(opponentHandPrefab, opponentsParent);
-            OpponentHandManager manager = newOpponent.GetComponent<OpponentHandManager>();
-            OpponentUI ui = newOpponent.GetComponent<OpponentUI>(); // script xử lý tên/avatar nếu bạn có
+        // foreach (var data in opponentsData)
+        // {
+        //     // GameObject newOpponent = Instantiate(opponentHandPrefab, opponentsParent);
+        //     // OpponentHandManager manager = newOpponent.GetComponent<OpponentHandManager>();
+        //     // OpponentUI ui = newOpponent.GetComponent<OpponentUI>(); // script xử lý tên/avatar nếu bạn có
 
-            if (ui != null)
-                ui.Setup(data); // truyền dữ liệu riêng vào UI
+        //     // if (ui != null)
+        //     //     ui.Setup(data); // truyền dữ liệu riêng vào UI
 
-            opponentManagers.Add(manager);
-        }
+        //     // opponentManagers.Add(manager);
+        //     GameObject newOpponent = Instantiate(opponentHandPrefab, opponentsParent);
+        //     PlayerHandManager manager = newOpponent.GetComponent<PlayerHandManager>();
+        //     OpponentUI ui = newOpponent.GetComponent<OpponentUI>(); // script xử lý tên/avatar nếu bạn có
+
+        //     if (ui != null)
+        //         ui.Setup(data); // truyền dữ liệu riêng vào UI
+
+        //     playerManagers.Add(manager);
+        // }
     }
 
     void DealInitialCards()
@@ -42,13 +55,13 @@ public class GameSetupManager : MonoBehaviour
             if (card != null) playerHandManager.SpawnCard(card);
         }
 
-        foreach (var opponent in opponentManagers)
-        {
-            for (int i = 0; i < 7; i++)
-            {
-                var card = DeckManager.Instance.DrawCard();
-                if (card != null) opponent.SpawnCard(card);
-            }
-        }
+        // foreach (var opponent in playerManagers)
+        // {
+        //     for (int i = 0; i < 7; i++)
+        //     {
+        //         var card = DeckManager.Instance.DrawCard();
+        //         if (card != null) opponent.SpawnCard(card);
+        //     }
+        // }
     }
 }
