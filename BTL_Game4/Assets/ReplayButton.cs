0c99e4b3f686dd768b0bd52f72447cecc0b25641
HQL04 <hoquanglong04@gmail.com>
2025-04-17 01:46:12 +0700

    17/04/2025

--- BTL_Game4/Assets/ReplayButton.cs
+++ BTL_Game4/Assets/ReplayButton.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
-using UnityEngine.EventSystems;  // Để làm việc với EventTrigger
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
 
 public class ReplayButton : MonoBehaviour, IPointerClickHandler
 {
-    // Phương thức này sẽ được gọi khi click vào image
+    public GameObject targetImage;  // Kéo Image con vào đây trong Inspector
+    private Vector3 originalScale;
+
     void Start()
     {
+        if (targetImage != null)
+        {
+            originalScale = targetImage.transform.localScale;
+        }
+
+        // Ẩn cha (Replay), kéo theo ẩn luôn con
         gameObject.SetActive(false);
     }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Hello");
+
+        if (targetImage != null)
+        {
+            targetImage.transform.localScale = originalScale * 0.9f;
+            Invoke("RestoreScale", 0.2f);
+        }
+    }
+
+    private void RestoreScale()
+    {
+        if (targetImage != null)
+        {
+            targetImage.transform.localScale = originalScale;
+        }
+        SceneManager.LoadScene("Menu");
     }
 
     public void ShowReplayButton()
